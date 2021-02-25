cf3cc8e9545e7458b3ebff3329b729ddc7598ddd
Stephen Bolton <stephen@bitsonthemind.com>
2021-02-24 21:30:17 -0700

    Remove elixir boilerplate

--- lib/reform.ex
+++ lib/reform.ex
@@ -1,18 +1,2 @@
 defmodule Reform do
-  @moduledoc """
-  Documentation for `Reform`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Reform.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/reform_test.exs
+++ test/reform_test.exs
@@ -1,8 +1,3 @@
 defmodule ReformTest do
   use ExUnit.Case
-  doctest Reform
-
-  test "greets the world" do
-    assert Reform.hello() == :world
-  end
 end
